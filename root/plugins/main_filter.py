4454d7fd012d37d321b39c3a15205f5c27131960
Angel0Lucifer <100468817+Angel0Lucifer@users.noreply.github.com>
2022-07-14 21:47:49 +0530

    Update main_filter.py

--- root/plugins/main_filter.py
+++ root/plugins/main_filter.py
@@ -1,8 +1,8 @@
 '''
 RenameBot
-This file is a part of mrvishal2k2 rename repo 
+This file is a part of holy angel rename repo 
 Dont kang !!!
-© Mrvishal2k2
+© HolyAngel
 '''
 import logging
 from pyrogram import Client, filters
