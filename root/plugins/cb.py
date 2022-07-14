e76a68b03f182b404c266164680f819f117a70c2
Angel0Lucifer <100468817+Angel0Lucifer@users.noreply.github.com>
2022-07-14 21:40:39 +0530

    Update cb.py

--- root/plugins/cb.py
+++ root/plugins/cb.py
@@ -1,8 +1,8 @@
 '''
 RenameBot
-This file is a part of mrvishal2k2 rename repo 
+This file is a part of holyangel rename repo 
 Dont kang !!!
-© Mrvishal2k2
+© HolyAngel
 '''
 import logging, asyncio
 from pyrogram import Client, filters
